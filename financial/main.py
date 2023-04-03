f988854b0759598eeabaf260ce6e927f81d14e76
Ilia Pasechnikov <ilia.pasechnikov@gmail.com>
2023-04-03 23:59:38 +0900

    Make root modules visibles from financial/main.py

--- financial/main.py
+++ financial/main.py
@@ -1,12 +1,22 @@
+# isort: off
+#
+# Dirty hack to make config.py importable
+# There is better solution to this problem by rearranging models, but I try to
+# keep directory structure as close to the task description as possible
+import sys
+sys.path.append('../python_assignment')
+# isort: on
+
 import uvicorn
 from fastapi import FastAPI
+
 from config import settings
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> dict:
     return {"message": "Hello World"}
 
 
